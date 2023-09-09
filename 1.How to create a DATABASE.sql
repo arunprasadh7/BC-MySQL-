f01cbe924eecadc50051d7a36a14d2f06022f3c2
arunprasadh7 <m.arunprasadh96@gmail.com>
2023-09-09 20:57:30 +0530

    Creating DB

---
+++ 1.How to create a DATABASE.sql
@@ -0,0 +1,11 @@
+-- How to create a database
+create database brodb;
+
+-- to use particular db
+use brodb;
+
+-- setting db to read only
+alter database brodb READ only = 1;
+
+-- disabling read only mode
+alter database brodb read only = 0;
